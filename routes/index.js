0c1997710afeb5457a59bec9854ac91fb89a1269
Ryan Sy <ryanbsy@gmail.com>
2019-04-03 18:43:24 -0400

    added sendgrid

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req);
   let showsArray;
   Post.find(function(err, posts) {
     if (err) {
@@ -215,6 +216,26 @@ router.post('/search', function(req, res) {
       }
     }
   });
+});
+
+router.post('/forgot', function(req, res) {
+  // using SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  /*
+  to do:
+  set random token using crypto, then set it to expire
+  */
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: req.body.email,
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
+  res.send('Check your email for password reset instructions.');
 });
 
 module.exports = router;
