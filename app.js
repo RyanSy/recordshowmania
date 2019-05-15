4a60581cfca759b34e24b4772c87fe9743e1f5cd
Ryan Sy <ryanbsy@gmail.com>
2019-05-15 00:24:13 -0400

    installed compression

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 var createError = require('http-errors');
+var compression = require('compression');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -39,6 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
 
 // session setup
 var sess = {
