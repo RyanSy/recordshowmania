d2fd6d3b452ad052dcb16ec238939fdf145d33bb
Ryan Sy <ryanbsy@gmail.com>
2024-12-27 16:03:33 -0500

    show more button on front page added

---
+++ public/javascripts/show-more.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.show:gt(20)').hide().last().after(
+        $('<div class="mt-3 pt-3 text-center"><input type="button" class="btn btn-primary" value="Show More" /></div>').click(function() {
+            $('.show:not(:visible):lt(20)').show();
+            if ($('.show:not(:visible)').length == 0) {
+                $(this).hide();
+            }            
+        })
+    );
+});
