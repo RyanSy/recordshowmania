6fe263bb22da3d61790c257f022db613c8691238
ryansy <ryanbsy@gmail.com>
2025-07-12 00:27:07 -0400

    layout updates

--- public/javascripts/show-more.js
+++ public/javascripts/show-more.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.show:gt(20)').hide().last().after(
-        $('<div class="mt-3 pt-3 text-center"><input type="button" class="btn btn-primary" value="Show More" /></div>').click(function() {
+        $('<div class="mt-3 pt-3 text-center"><input type="button" class="btn btn-success" value="Show More" /></div>').click(function() {
             $('.show:not(:visible):lt(20)').show();
             if ($('.show:not(:visible)').length == 0) {
                 $(this).hide();
